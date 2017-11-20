8c15a8d510359f1bba509c3ffc0f32bc17da3a7b
Habib Khan <habibmkhan92@gmail.com>
2017-11-19 22:19:08 -0800

    sync solution

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,7 +1,6 @@
 'use strict'
 
 module.exports = (logSources, printer) => {
-
-	console.log('logSources', logSources);
+	// console.log('logSources', logSources);
 	// throw new Error('Not implemented yet!  That part is up to you!')
 }
--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,5 +1,31 @@
 'use strict'
 
+const _ = require('lodash')
+const FastPriorityQueue = require("fastpriorityqueue");
+
 module.exports = (logSources, printer) => {
+	let polledSource;
+	let fastLogQueue = new FastPriorityQueue((a, b) => {
+		return a.date < b.date;
+	});
+
+	let storage = [];
+	let current = logSources[0].last.date || '';
+	logSources.forEach((source) => {
+		// logQueue.queue(source.last.date);
+		fastLogQueue.add(source.last);
+		storage.push(source);
+		if (current < source.last.date) {
+			current = source.last.date;
+		}
+	});
+	// console.log('printing logQueue', logQueue);
+	while (!fastLogQueue.isEmpty()) {
+		polledSource = fastLogQueue.poll();
+		if (_.isDate(polledSource.date)) {
+			printer.print(polledSource);
+		}
+	}
+	printer.done();
 	// throw new Error('Not implemented yet!  That part is up to you!')
 }
