58c24fc0665c855661efb3a83b2c5feffd74d584
Habib Khan <habibmkhan92@gmail.com>
2017-11-19 23:58:19 -0800

    reduce code on sync file

--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -8,12 +8,8 @@ module.exports = (logSources, printer) => {
 	let fastLogQueue = new FastPriorityQueue((a, b) => {
 		return a.date < b.date;
 	});
-	let current = logSources[0].last.date || '';
 	logSources.forEach((source) => {
 		fastLogQueue.add(source.last);
-		if (current < source.last.date) {
-			current = source.last.date;
-		}
 	});
 	while (!fastLogQueue.isEmpty()) {
 		polledSource = fastLogQueue.poll();
