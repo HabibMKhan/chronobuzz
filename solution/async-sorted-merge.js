767f189cabe715ebc6e571da1e9597516242793e
Habib Khan <habibmkhan92@gmail.com>
2017-11-19 23:49:09 -0800

    added async solution

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,6 +1,30 @@
 'use strict'
 
-module.exports = (logSources, printer) => {
-	// console.log('logSources', logSources);
-	// throw new Error('Not implemented yet!  That part is up to you!')
+const _ = require('lodash')
+const FastPriorityQueue = require("fastpriorityqueue");
+
+module.exports = async (logSources, printer) => {
+	let polledSource;
+	let fastLogQueue = new FastPriorityQueue((a, b) => {
+    return a.date < b.date;
+	});
+  let current = null;
+  let currentSource;
+  for (let i = 0; i < logSources.length; i++) {
+    current = logSources[i];
+    currentSource = null;
+    while (currentSource !== false) {
+        currentSource = await current.popAsync();
+        if (currentSource) {
+          fastLogQueue.add(currentSource);
+        }
+    }
+  }
+	while (!fastLogQueue.isEmpty()) {
+		polledSource = fastLogQueue.poll();
+		if (_.isDate(polledSource.date)) {
+			printer.print(polledSource);
+		}
+	}
+	printer.done();
 }
--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -8,18 +8,13 @@ module.exports = (logSources, printer) => {
 	let fastLogQueue = new FastPriorityQueue((a, b) => {
 		return a.date < b.date;
 	});
-
-	let storage = [];
 	let current = logSources[0].last.date || '';
 	logSources.forEach((source) => {
-		// logQueue.queue(source.last.date);
 		fastLogQueue.add(source.last);
-		storage.push(source);
 		if (current < source.last.date) {
 			current = source.last.date;
 		}
 	});
-	// console.log('printing logQueue', logQueue);
 	while (!fastLogQueue.isEmpty()) {
 		polledSource = fastLogQueue.poll();
 		if (_.isDate(polledSource.date)) {
@@ -27,5 +22,4 @@ module.exports = (logSources, printer) => {
 		}
 	}
 	printer.done();
-	// throw new Error('Not implemented yet!  That part is up to you!')
 }
